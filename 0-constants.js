37311ec1a4f0e99a32a37179bbfd0ee9f54b64b4
katman-Generation <tkatema5@gmail.com>
2022-12-19 11:30:27 +0200

    submitting task 0

---
+++ 0-constants.js
@@ -0,0 +1,14 @@
+export function taskFirst() {
+    const task = 'I prefer const when i can.';
+}
+
+export function getLast() {
+    return ' is okay';
+}
+
+export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+
+    return combination;
+}
